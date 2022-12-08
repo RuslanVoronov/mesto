0cd3473185249fd538bd6cdbd9ce4b9cbd8819ef
DESKTOP-3U5UA8L\voron <voronovrv.99@mail.ru>
2022-12-08 20:58:20 +0300

    удаление лишнего кода

--- src/pages/index.js
+++ src/pages/index.js
@@ -124,10 +124,3 @@ profileEditPopup.setEventListeners();
 // Слушатели
 popupEditButton.addEventListener('click', openProfilePopup);
 cardAddButton.addEventListener('click', openCardPopup);
-
-const numbers = [2, 3, 5];
-
-// Стрелочная функция. Не запнётся ли на ней Internet Explorer?
-const doubledNumbers = numbers.map(number => number * 2);
-
-console.log(doubledNumbers); // 4, 6, 10
